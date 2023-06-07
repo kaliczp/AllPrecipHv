b5eeba4ca04072cb34ed99562b7b74acaaef1d31
Péter Kalicz <kaliczp@gmail.com>
2023-06-07 17:46:26 +0200

    A possibility to read and identify file structure

--- import.R
+++ import.R
@@ -1,3 +1,10 @@
 for(ttev in 2010:2023) {
-    print(dir(paste0("Hidegvíz/", ttev, "/Hellmann"), patt = "^csap")[1])
+    csapfilename <- dir(paste0("Hidegvíz/", ttev, "/Hellmann"), patt = "^csap", full.names = TRUE)[1]
+    print(ttev)
+    print(scan(file=csapfilename, what = character(), sep = "\n"))
+}
+
+read.prec <- function(x) {
+    forsep <- scan(file=csapfilename, what = character(), sep = "\n", n = 2)
+    print(grep(";", forsep))
 }
