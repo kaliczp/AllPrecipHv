bd444fd5009b43b611ebd015f89a7c4c5f5e0a21
Kalicz Péter <kaliczp@gmail.com>
2024-06-10 15:34:48 +0200

    A little bit extended time intervall

--- sametimewindow.R
+++ sametimewindow.R
@@ -1,7 +1,7 @@
 csapsum.df <- data.frame(Év = numeric(14), Pmm = numeric(14))
 for(ttev in 2010:2023) {
     csapsum.df[ttev - 2009, "Év"]  <- ttev
-    csapsum <- sum(csap[paste0(ttev,"-01-01/",ttev,"-06-05")], na.rm = TRUE)
+    csapsum <- sum(csap[paste0(ttev,"-01-01/",ttev,"-06-10")], na.rm = TRUE)
     csapsum.df[ttev - 2009, "Pmm"]  <- csapsum
 }
 
@@ -15,6 +15,6 @@ for(ttev in 2010:2023) {
 csapsum.df <- data.frame(Év = numeric(14), Pmm = numeric(14))
 for(ttev in 2010:2023) {
     csapsum.df[ttev - 2009, "Év"]  <- ttev
-    csapsum <- sum(csap[paste0(ttev,"-04-01/",ttev,"-06-05")], na.rm = TRUE)
+    csapsum <- sum(csap[paste0(ttev,"-04-01/",ttev,"-06-10")], na.rm = TRUE)
     csapsum.df[ttev - 2009, "Pmm"]  <- csapsum
 }
